8b992e7bb6ef9784f3d15bff61317406ab75c338
gmdrax <gerard.maestre.7e9@itb.cat>
2026-05-14 18:18:37 +0200

    Fix: Add missing Button import for Desktop compile

--- desktopApp/src/main/kotlin/Main.kt
+++ desktopApp/src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.Button
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
